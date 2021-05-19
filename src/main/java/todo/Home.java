5cd90e88f5dcc2b4bf910a33b97614b076ce717f
Node-23 <matheusvinicius38@live.com>
2021-05-19 11:45:36 -0300

    Project structure created

--- src/main/java/todo/Home.java
+++ src/main/java/todo/Home.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,6 +15,13 @@ public class Home extends Application {
             Parent parent = FXMLLoader.load(getClass().getResource("Home.fxml"));
             Scene scene = new Scene(parent);
             stage.setScene(scene);
+            stage.setTitle("TODO");
+
+            //TODO fix bug: Icon is not showing
+            Image icon = new Image(getClass().getResourceAsStream("Icon.png"));
+            stage.getIcons().add(icon);
+
+            
             stage.show();
         } catch (Exception e) {
             System.out.println(e);
